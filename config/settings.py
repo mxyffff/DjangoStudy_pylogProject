62495dd7b79b16e37f96dabd78638c4da7bc914c
mxyffff <rua0254@naver.com>
2025-04-12 00:21:46 +0900

    chap11까지 실습완

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from django.conf.global_settings import TEMPLATES
+from django.conf.global_settings import TEMPLATES, STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +107,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [ BASE_DIR / "static", ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from config.views import index # index import
@@ -9,3 +10,7 @@ urlpatterns = [
     path("", index), # 경로가 없을 때 index View 연결
     path("posts/", post_list),
 ]
+
+# 개발 환경일 때만 static 파일 제공
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
